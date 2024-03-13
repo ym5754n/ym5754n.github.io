f9f46d7cb66a87fb8b4799967cb1d476ea5a771d
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-03-14 01:54:26 +0900

    ブログポストを表示

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,38 @@
+import { client } from "@/lib/client";
+import { Blog } from "@/types/blog";
+import markdownHtml from "zenn-markdown-html";
+import 'zenn-content-css';
+
+export async function generateStaticParams({}) {
+  const posts = await client.get({ endpoint: "blog" });
+ 
+  return posts.contents.map((post: Blog) => ({
+    id: post.id,
+  }))
+}
+
+export default async function Post({ params }: { params: { id: string }}) {
+  const post: Blog = await getData(params.id);
+
+  let html = markdownHtml(post.content);
+
+  return (
+    <main>
+      <h1>{post.title}</h1>
+      <p>{post.publishedAt}</p>
+      <div
+        className="znc"
+        dangerouslySetInnerHTML={{
+          __html: html,
+        }}
+      />
+    </main>
+  );
+
+}
+
+async function getData(id: string) {
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  return data;
+}
