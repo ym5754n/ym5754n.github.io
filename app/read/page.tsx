f6b30e71cbe8a1e8c154687d915f30d5b6a027fc
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-06-18 23:47:51 +0900

    書籍情報を表示

--- app/read/page.tsx
+++ app/read/page.tsx
@@ -1,14 +1,59 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+import { client } from "@/lib/client";
+import { Book } from "@/types/book";
+import Image from "next/image";
 
-export default function Read() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default async function Read() {
+  const books: Book[] = await getData();
+
   return (
-    <p>{`This is the list ym5754n read.`}</p>
+    <div>
+      <ul className="grid grid-flow-row gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {books.map((book, key) => (
+          <li key={key} className="min-w-0">
+            <Card className="min-h-full">
+              <CardHeader>
+                <CardDescription>読了日：{new Date(book.date).toLocaleDateString()}</CardDescription>
+                <CardTitle>{book.book.volumeInfo.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+              <div style={{ display: "flex", justifyContent: "center"}}>
+                <Image
+                  src={book.book.volumeInfo.imageLinks.smallThumbnail}
+                  alt={book.book.volumeInfo.title}
+                  width={150}
+                  height={100}
+                />
+              </div>
+              </CardContent>
+              <CardFooter>
+                <p>{book.comment}</p>
+              </CardFooter>
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
+}
+
+async function getData() {
+  const data = await client.get({
+    endpoint: "book",
+    queries: { orders: '-date' },
+  });
+
+  if (!data.contents) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return data.contents;
 }
---
+++ types/book.ts
@@ -0,0 +1,17 @@
+export type Book = {
+    id: string,
+    createdAt: string,
+    updatedAt: string,
+    publishedAt: string,
+    revisedAt: string,
+    date: string,
+    comment: string,
+    book: {
+        volumeInfo: {
+            title: string,
+            imageLinks: {
+                smallThumbnail: string,
+            },
+        },
+    }
+}
